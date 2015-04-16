82b22b82cce920f84b65fef8d4063f9804f46cc4
RafaelAnt <rafael.uminho@hotmail.com>
2015-04-16 12:04:57 +0100

    Implementado a class "Campo"

--- TP2-CC-Music Server/src/CalculadoresResposta/Campo.java
+++ TP2-CC-Music Server/src/CalculadoresResposta/Campo.java
@@ -5,16 +5,31 @@
  */
 package CalculadoresResposta;
 
+import java.nio.ByteBuffer;
+
 /**
  *
  * @author Rafael
  */
 public class Campo {
-    private byte campo;
-    private byte size;
-    private String dados;
 
-    public Campo(byte campo, byte size, String dados) {
+    private byte campo;
+    private short size;
+    private byte[] dados;
+
+    public Campo() {
+        this.campo = -1;
+        this.size = 0;
+        this.dados = null;
+    }
+
+    public Campo(byte campo, short size, String dados) {
+        this.campo = campo;
+        this.size = size;
+        this.dados = dados.getBytes();
+    }
+
+    public Campo(byte campo, short size, byte[] dados) {
         this.campo = campo;
         this.size = size;
         this.dados = dados;
@@ -24,26 +39,49 @@ public class Campo {
         return campo;
     }
 
-    public byte getSize() {
-        return size;
-    }
-
-    public String getDados() {
-        return dados;
-    }
-
     public void setCampo(byte campo) {
         this.campo = campo;
     }
 
-    public void setSize(byte size) {
+    public short getSize() {
+        return size;
+    }
+
+    public void setSize(short size) {
         this.size = size;
     }
 
-    public void setDados(String dados) {
+    public byte[] getDados() {
+        byte[] resp = new byte[this.size];
+        System.arraycopy(this.dados, 0, resp, 0, this.size);
+        return resp;
+    }
+
+    public void setDados(byte[] dados, short size) {
+        setSize(size);
+        System.arraycopy(dados, 0, this.dados, 0, size);
         this.dados = dados;
     }
     
-    
-    
+    public short getTotalSize(){
+        return (short) (this.size+3);
+    }
+
+
+    public byte[] generate() {
+        byte[] resp = new byte[this.size + 3];
+        resp[0] = this.campo;
+        byte[] buff = shortToByte(this.size);
+        resp[1] = buff[0];
+        resp[2] = buff[1];
+        System.arraycopy(this.dados, 0, resp, 3, this.size);
+        
+       return resp;
+    }
+
+    public byte[] shortToByte(short size) {
+        byte[] bytes = ByteBuffer.allocate(2).putShort(size).array();
+        return bytes;
+    }
+
 }
