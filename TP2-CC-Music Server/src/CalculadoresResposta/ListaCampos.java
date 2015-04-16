bb2eb35b3904ebffca643af7fa30239e78ae5224
NunoOliv <nuno.oliv.94@gmail.com>
2015-04-16 12:16:46 +0100

    Conclusão ListaCampos

--- TP2-CC-Music Server/src/CalculadoresResposta/ListaCampos.java
+++ TP2-CC-Music Server/src/CalculadoresResposta/ListaCampos.java
@@ -5,44 +5,48 @@
  */
 package CalculadoresResposta;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Rafael
  */
 public class ListaCampos {
-    private byte campo;
-    private byte size;
-    private String dados;
+    private final ArrayList<Campo> lista;
+    private short totalSize;
 
-    public ListaCampos(byte campo, byte size, String dados) {
-        this.campo = campo;
-        this.size = size;
-        this.dados = dados;
+    public ListaCampos(ArrayList<Campo> lista) {
+        this.lista = lista;
     }
 
-    public byte getCampo() {
-        return campo;
+    public ListaCampos() {
+        this.lista = new ArrayList<>();
+    }
+    
+    public boolean addCampo(Campo campo) {
+        totalSize += campo.getTotalSize();
+        return this.lista.add(campo);
     }
 
-    public byte getSize() {
-        return size;
+    public short getTotalSize() {
+        return totalSize;
     }
-
-    public String getDados() {
-        return dados;
+    
+    public byte getNCampos() {
+        return (byte)lista.size();
     }
-
-    public void setCampo(byte campo) {
-        this.campo = campo;
+    
+    public byte[] generate() {
+        byte[] ret = new byte[totalSize];
+        int currentSize = 0;
+        
+        for(Campo campo : lista ) {
+            System.arraycopy(campo.generate(), 0, ret, currentSize, campo.getTotalSize() );
+            currentSize = currentSize+campo.getTotalSize();
+        }
+        return ret; 
     }
-
-    public void setSize(byte size) {
-        this.size = size;
-    }
-
-    public void setDados(String dados) {
-        this.dados = dados;
-    }
+    
     
     
     
