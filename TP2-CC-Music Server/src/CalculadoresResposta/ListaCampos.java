2a600da167a5e9aa96575acba9a63b275aeb564d
NunoOliv <nuno.oliv.94@gmail.com>
2015-04-16 12:08:57 +0100

    ListaCampos

---
+++ TP2-CC-Music Server/src/CalculadoresResposta/ListaCampos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalculadoresResposta;
+
+/**
+ *
+ * @author Rafael
+ */
+public class ListaCampos {
+    private byte campo;
+    private byte size;
+    private String dados;
+
+    public ListaCampos(byte campo, byte size, String dados) {
+        this.campo = campo;
+        this.size = size;
+        this.dados = dados;
+    }
+
+    public byte getCampo() {
+        return campo;
+    }
+
+    public byte getSize() {
+        return size;
+    }
+
+    public String getDados() {
+        return dados;
+    }
+
+    public void setCampo(byte campo) {
+        this.campo = campo;
+    }
+
+    public void setSize(byte size) {
+        this.size = size;
+    }
+
+    public void setDados(String dados) {
+        this.dados = dados;
+    }
+    
+    
+    
+}
