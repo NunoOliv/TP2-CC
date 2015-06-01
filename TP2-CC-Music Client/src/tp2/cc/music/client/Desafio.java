3fbec73f86f9daf58aa458e68350f3123f3d59da
Rafael Antunes <rafael.uminho@hotmail.com>
2015-06-01 21:03:22 +0100

    Melhoria da data.. (Inacabado)

--- TP2-CC-Music Client/src/tp2/cc/music/client/Desafio.java
+++ TP2-CC-Music Client/src/tp2/cc/music/client/Desafio.java
@@ -6,6 +6,7 @@ public class Desafio {
 
     private String nome;
     private String data, hora;
+    private int ano, mes, dia, hor, min, seg;
     private ArrayList<Pergunta> perguntas;
 
     public Desafio() {
@@ -13,6 +14,12 @@ public class Desafio {
         this.nome = "";
         this.data = "";
         this.hora = "";
+        this.ano = 0;
+        this.mes = 0;
+        this.dia = 0;
+        this.hor = 0;
+        this.min = 0;
+        this.seg = 0;
     }
 
     public Desafio(ArrayList<Pergunta> perguntas) {
@@ -20,6 +27,12 @@ public class Desafio {
         this.nome = "";
         this.data = "";
         this.hora = "";
+        this.ano = 0;
+        this.mes = 0;
+        this.dia = 0;
+        this.hor = 0;
+        this.min = 0;
+        this.seg = 0;
     }
 
     public Desafio(int nMaxPerguntas) {
@@ -31,6 +44,12 @@ public class Desafio {
         this.nome = "";
         this.data = "";
         this.hora = "";
+        this.ano = 0;
+        this.mes = 0;
+        this.dia = 0;
+        this.hor = 0;
+        this.min = 0;
+        this.seg = 0;
     }
 
     public boolean addPergunta(Pergunta p) {
@@ -61,7 +80,15 @@ public class Desafio {
         return data;
     }
 
-    public void setData(String data) {
+    public void setData(String data) throws Exception {
+        if (data.length() != 6) {
+            throw new Exception("Data inválida!");
+        }
+        try {
+            Integer.parseInt(data);
+        } catch (Exception e) {
+            throw new Exception("Data inválida!");
+        }
         this.data = data;
     }
 
@@ -69,8 +96,170 @@ public class Desafio {
         return hora;
     }
 
-    public void setHora(String hora) {
+    public void setHora(String hora) throws Exception {
+        if (hora.length() != 6) {
+            throw new Exception("Hora inválida!");
+        }
+        try {
+            Integer.parseInt(hora);
+        } catch (Exception e) {
+            throw new Exception("Hora inválida!");
+        }
         this.hora = hora;
     }
 
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) throws Exception {
+        if (ano < 2015) {
+            throw new Exception("Ano inválido!");
+        }
+        this.ano = ano;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) throws Exception {
+        if (mes < 0 || mes > 12) {
+            throw new Exception("Mês inválido!");
+        }
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) throws Exception {
+        if (dia < 0 || dia > 31) {
+            throw new Exception("Dia inválido!");
+        }
+        this.dia = dia;
+    }
+
+    public int getHor() {
+        return hor;
+    }
+
+    public void setHor(int hor) throws Exception {
+        if (hor < 0 || hor > 24) {
+            throw new Exception("Hora inválida!");
+        }
+        this.hor = hor;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) throws Exception {
+        if (min < 0 || min > 60) {
+            throw new Exception("Minuto inválido!");
+        }
+        this.min = min;
+    }
+
+    public int getSeg() {
+        return seg;
+    }
+
+    public void setSeg(int seg) throws Exception {
+        if (seg < 0 || seg > 60) {
+            throw new Exception("Segundo inválido!");
+        }
+        this.seg = seg;
+    }
+
+    /**
+     * data de String Format para Integer Format (vários inteiros)
+     *
+     * @return
+     * @throws java.lang.Exception
+     */
+    public boolean dateSFtoIF() throws Exception {
+        if (this.data.equals("") || this.hora.equals("")) {
+            throw new Exception("Data ou Hora não definida no formato String!");
+        }
+
+        ano = 2000 + Integer.parseInt(data.substring(0, 2));
+        mes = Integer.parseInt(data.substring(2, 4));
+        dia = Integer.parseInt(data.substring(4));
+        if (ano < 2015 || mes < 0 || dia < 0 || mes > 12 || dia > 31) {
+            this.ano = 0;
+            this.mes = 0;
+            this.dia = 0;
+            throw new Exception("Data inválida!");
+        }
+
+        hor = Integer.parseInt(hora.substring(0, 2));
+        min = Integer.parseInt(hora.substring(2, 4));
+        seg = Integer.parseInt(hora.substring(4));
+        if (hor < 0 || hor > 24 || min < 0 || seg < 0 || min > 60 || seg > 60) {
+            this.hor = 0;
+            this.min = 0;
+            this.seg = 0;
+            throw new Exception("Hora inválida!");
+        }
+
+        return true;
+    }
+
+    public boolean dateIFtoSF() throws Exception {
+        if (ano < 2015) {
+            throw new Exception("Ano inválido!");
+        }
+        if (mes < 0 || mes > 12) {
+            throw new Exception("Mês inválido!");
+        }
+        if (dia < 0 || dia > 31) {
+            throw new Exception("Dia inválido!");
+        }
+        if (hor < 0 || hor > 24) {
+            throw new Exception("Hora inválida!");
+        }
+        if (min < 0 || min > 60) {
+            throw new Exception("Minuto inválido!");
+        }
+        if (seg < 0 || seg > 60) {
+            throw new Exception("Segundo inválido!");
+        }
+
+        data = "";
+        hora = "";
+
+        if (ano < 10) {
+            data = "0";
+            data = data.concat(Integer.toString(ano));
+        } else {
+            data = Integer.toString(ano);
+        }
+        if (mes < 10) {
+            data = data.concat("0");
+        }
+        data = data.concat(Integer.toString(mes));
+        if (dia < 10) {
+            data = data.concat("0");
+        }
+
+        if (hor < 10) {
+            hora = "0";
+            hora = hora.concat(Integer.toString(hor));
+        } else {
+            hora = Integer.toString(hor);
+        }
+        if (min < 10) {
+            hora = hora.concat("0");
+        }
+        hora = hora.concat(Integer.toString(min));
+        if (seg < 10) {
+            hora = hora.concat("0");
+        }
+        hora = hora.concat(Integer.toString(seg));
+        
+        return true;
+    }
 }
--- TP2-CC-Music Server/src/tp2/cc/music/ServerUDP.java
+++ TP2-CC-Music Server/src/tp2/cc/music/ServerUDP.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tp2.cc.music;
 
 import DataBase.Desafio;
@@ -23,7 +18,6 @@ import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
  *
